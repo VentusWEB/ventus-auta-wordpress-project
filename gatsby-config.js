d59f5ad8b258ea1d5dd070d732cbd0477677c19d
Felikso <aleksandrasniegucka@wp.pl>
2021-07-18 14:52:29 +0200

    yandex

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,49 +26,6 @@ module.exports = {
 				siteUrl: process.env.SITE_URL,
 			},
 		},
-		/* {
-			resolve: `gatsby-plugin-sitemap`,
-			options: {
-				query: `{
-				site {
-					siteMetadata {
-					  siteUrl
-					}
-				  }
-				  allSitePage {
-					edges {
-					  node {
-						path
-					  }
-					}
-				  }
-			  }`,
-				serialize: ({ site, allSitePage }) => {
-					let pages = []
-					allSitePage.edges.map(edge => {
-						pages.push({
-							url: site.siteMetadata.siteUrl + edge.node.path,
-							changefreq: `daily`,
-							priority: 0.7,
-						})
-					})
-					return pages
-				},
-			},
-		}, */
-
-		/* 		{
-					resolve: "gatsby-source-graphql",
-					options: {
-						typeName: "GitHub",
-						fieldName: "github",
-						url: "https://api.github.com/graphql",
-						headers: {
-							Authorization: `bearer ${process.env.PORTFOLIO_GITHUB_TOKEN}`,
-						},
-						fetchOptions: {},
-					},
-				}, */
 		{
 			resolve: "gatsby-plugin-nprogress",
 			options: {
@@ -106,7 +63,6 @@ module.exports = {
 		},
 		`gatsby-transformer-sharp`,
 		`gatsby-transformer-json`,
-		'gatsby-plugin-sitemap',
 		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-plugin-manifest`,
@@ -127,65 +83,6 @@ module.exports = {
 							offset: -100
 						} */
 		},
-		/*  {
-		   resolve: `gatsby-plugin-cookie-though`,
-		   options: {
-			 config: {
-			   policies: [
-				 {
-				   id: "essential",
-				   label: "Niezbędne ciasteczka",
-				   description:
-					 "Są one konieczne do właściwego funkcjonowania witryny internetowej.",
-				   category: "essential",
-				 },
-				 {
-				   id: "functional",
-				   label: "Funkcjonalne ciasteczka",
-				   category: "functional",
-				   description:
-					 "Pomagają nam w sprawdzaniu efektywności wdrażanych przez nas działań marketingowych, dostosowując je do indywidualnych preferencji użytkownika.",
-				 },
-				 {
-				   id: "statistics",
-				   label: "Analityczne ciasteczka",
-				   category: "statistics",
-				   description:
-					 "Dzięki nim możemy dowiedzieć się, co cieszy się największym zainteresowaniem na naszych podstronach.",
-				 },
-				 {
-				   id: "social",
-				   label: "Społecznościowe ciasteczka",
-				   category: "social",
-				   description:
-					 "Wykorzystujemy je w celu poprawienia efektywności podejmowanych przez nas działań marketingowych.",
-				 },
-			   ],
-			   permissionLabels: {
-				 accept: "Zaakceptuj",
-				 acceptAll: "Zaakceptuj wszystkie",
-				 decline: "Odrzuć",
-			   },
-			   cookiePreferenceKey: "cookie-preferences",
-			   header: {
-				 title: "Ciasteczka?",
-				 subTitle: "Znowu to okienko...",
-				 description:
-				   "Każdy z pewnością chce pokazać się od jak najlepszej strony, również i my. Wykorzystanie ciasteczek daje nam na to gwarancję.",
-			   },
-			   cookiePolicy: {
-				 url: "http://localhost:8000/polityka-prywatnosci",
-				 label: "Przeczytaj całą deklarację",
-			   },
-			 },
-		   },
-		 }, */
-		/*     {
-			  resolve: `gatsby-plugin-sitemap`,
-			  options: {
-				output: `/sitemap.xml`,
-			  }
-			}, */
 		'gatsby-plugin-robots-txt',
 		`gatsby-plugin-polyfill-io`,
 		`gatsby-plugin-preact`,
