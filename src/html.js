67ab38f73786933ba8d45ddbb32e98c537a572f7
Felikso <aleksandrasniegucka@wp.pl>
2021-07-18 15:39:02 +0200

    yandex

--- src/html.js
+++ src/html.js
@@ -27,7 +27,8 @@ export default function HTML(props) {
                         __html: `
                         <!-- Yandex.Metrika counter -->
                         <script type="text/javascript" >
-                           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            setTimeout(function(){
+                                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
                         
@@ -35,8 +36,10 @@ export default function HTML(props) {
                                 clickmap:true,
                                 trackLinks:true,
                                 accurateTrackBounce:true,
-                                webvisor:true
+                                webvisor:true,
+                                ecommerce:"dataLayer"
                            });
+                           }, 5000);
                         </script>
                         <noscript><div><img src="https://mc.yandex.ru/watch/83027727" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
                         <!-- /Yandex.Metrika counter -->
